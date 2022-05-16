c2b8ab60113f14bbb989740b39318a001265e1d2
itsAdil45 <sheikhadil086@gmail.com>
2022-05-16 19:57:49 +0500

    Create-React-App

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">Amazona</a>
+      </header>
+      <main>List</main>
+    </div>
+  );
+}
+
+export default App;
