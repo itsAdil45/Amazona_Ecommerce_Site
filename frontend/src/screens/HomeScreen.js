037fde3c9b7d8646c8bc22c5584b4b9c810d2bb7
itsAdil45 <sheikhadil086@gmail.com>
2022-05-20 21:23:43 +0500

    implementing axios

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,13 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import data from "../Data";
 
+import axios from "axios";
 export default function HomeScreen() {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios.get("/api/products");
+      setProducts(result.data);
+    };
+    fetchData();
+  }, []);
   return (
     <div>
       <h1>Featured Products</h1>
       <div className="products">
-        {data.products.map((item) => (
+        {products.map((item) => (
           <div className="product" key={item.slug}>
             <Link to={`/product/${item.slug}`}>
               <img src={item.image} alt="tshirt" />
